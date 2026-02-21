8ab305fc2cccfed564db95121b29d9a46edbfe5e
naraenbalaji <97966134+naraenbalaji@users.noreply.github.com>
2026-02-21 14:37:46 +0530

    Update db_config.py

--- Backend/db_config.py
+++ Backend/db_config.py
@@ -1,12 +1,13 @@
 import psycopg2
 
 DB_CONFIG = {
-    "host": "127.0.0.1",
-    "port":"5432",
-    "dbname":"invoice",
-    "user":"postgres",
-    "password":"abcdefgh",
+   "host": "aws-1-ap-northeast-1.pooler.supabase.com",
+"port": 6543,
+"database": "postgres",
+"user":"postgres.lcdiarvjzwumlthwosry",
+"sslmode":"require",
+    "password":"Srirama@2209",
 }
 
 
-INVOICE_PARENT = "/Users/naraenbalaji/Desktop/Whirldata Task/BE"
+INVOICE_PARENT = "/Users/naraenbalaji/Desktop/Whirldata Task/BE/uploads"
